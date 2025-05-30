422c77e92dce49c16193aff32234e6139e238778
camila <camila@gmail.com>
2025-05-30 12:04:12 -0400

    Add cards with bootstrap on cardpizza and home

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -1,23 +1,34 @@
+import { formatPrice } from '../assets/helpers/formatPrice';
+
 const CardPizza = (props) => {
   return (
     <div>
-
-        <img src={props.img} alt={props.name} />
-
-        <h2>
-            {props.name}
-        </h2>
-
-        <p>
-            {props.ingredients}
-        </p>
-
-        <p>
-            Price: ${props.price}
-        </p>
+      <div className="card" style={{ width: '18rem' }}>
+        <img src={props.img} className="card-img-top" alt={props.name} />
         
-    </div>
-  )
-}
+        <div 
+          className="card-body d-flex flex-column" 
+          style={{ minHeight: '320px' }} // ajusta a la altura que quieras para igualar
+        >
+          <h3 className="card-title text-center">{props.name}</h3>
+          <p className="card-text mb-1 fw-bold border-bottom">Ingredientes:</p>
+          <ul className="list-unstyled ms-3 flex-grow-1">
+            {props.ingredients.map((ingredient, index) => (
+              <li key={index} className="text-center border-bottom">🍕 {ingredient}</li>
+            ))}
+          </ul>
 
-export default CardPizza
+          <p className="card-text text-center mt-3">Price: {formatPrice(props.price)}</p>
+
+          <div className="d-flex gap-3 justify-content-center mt-auto">
+            <a href="#" className="btn btn-dark">Ver más</a>
+            <a href="#" className="btn btn-dark">Añadir</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export default CardPizza;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,57 @@
 import { formatPrice } from '../assets/helpers/formatPrice';
 
 const Navbar = () => {
-
   const total = 25000;
   const token = false;
 
   return (
-  <div>
-    <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand text-white" href="#">Pizzería Mamma Mia</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
+    <div>
+      <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand text-white" href="#">Pizzería Mamma Mia</a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link border border-white text-white rounded px-3" href="#">🍕 Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link border border-white text-white rounded px-3" href="#">🔐 Login</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link border border-white text-white rounded px-3" href="#">🔐 Register</a>
-            </li>
-          </ul>
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <a class="nav-link border border-info text-info rounded px-3" href="#">🛒 Total: {formatPrice(total)}</a>
-            </li>
-          </ul>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link border border-white text-white rounded px-3" href="#">🍕 Home</a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link border border-white text-white rounded px-3" href="#">
+                  {token ? '🔓 Profile' : '🔐 Login'}
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link border border-white text-white rounded px-3" href="#">
+                  {token ? '🔒 Logout' : '🔐 Register'}
+                </a>
+              </li>
+            </ul>
+
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link border border-info text-info rounded px-3" href="#">
+                  🛒 Total: {formatPrice(total)}
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  </div>
+      </nav>
+    </div>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
